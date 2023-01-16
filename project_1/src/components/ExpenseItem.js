fb1124ae3febea0e9924fe019600fedc86b625ba
Kayleigh Watts <kwatts949@gmail.com>
2023-01-16 12:49:59 +0000

    Add new expenses component

--- project_1/src/components/ExpenseItem.js
+++ project_1/src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
     </div>
   );
-}
+};
 
 export default ExpenseItem;
 // props == properties from app.js
