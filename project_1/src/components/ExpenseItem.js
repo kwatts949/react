24f1892187ca1be7887059cb766aa0023a78aaa1
Kayleigh Watts <kwatts949@gmail.com>
2023-01-16 10:43:02 +0000

    Add dynamic data output

--- project_1/src/components/ExpenseItem.js
+++ project_1/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 0, 18).toDateString();
+  const expenseTitle = 'String'
+  const expenseAmount = 0.00
+
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{expenseDate}</div>
       <div className='expense-item__description'>
-        <h2>Title</h2>
-      <div className='expense-item__price'>Amount</div>
+        <h2>{expenseTitle}</h2>
+      <div className='expense-item__price'>{expenseAmount}</div>
       </div>
     </div>
   );
