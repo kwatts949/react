4c92090244482db203916434f527a6d6186fe9fa
Kayleigh Watts <kwatts949@gmail.com>
2023-01-16 11:45:52 +0000

    Update expenses to use props

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -1,11 +1,52 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Make Up",
+      amount: "51.00",
+      date: new Date(2023, 0, 16),
+    },
+    { id: "e2", title: "Coat", amount: "225.00", date: new Date(2023, 0, 16) },
+    {
+      id: "e3",
+      title: "Make Up",
+      amount: "14.00",
+      date: new Date(2023, 0, 16),
+    },
+    {
+      id: "e4",
+      title: "Clothes",
+      amount: "80.10",
+      date: new Date(2023, 0, 17),
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>My App</h1>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem
+          title={expenses[0].title}
+          amount={expenses[0].amount}
+          date={expenses[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[1].title}
+          amount={expenses[1].amount}
+          date={expenses[1].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[2].title}
+          amount={expenses[2].amount}
+          date={expenses[2].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={expenses[3].title}
+          amount={expenses[3].amount}
+          date={expenses[3].date}
+        ></ExpenseItem>
       </header>
     </div>
   );
--- project_1/src/components/ExpenseItem.js
+++ project_1/src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 0, 18).toDateString();
-  const expenseTitle = 'String'
-  const expenseAmount = 0.00
-
+function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
-      <div>{expenseDate}</div>
-      <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-      <div className='expense-item__price'>{expenseAmount}</div>
+    <div className="expense-item">
+      <div>{props.date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">£{props.amount}</div>
       </div>
     </div>
   );
 }
 
 export default ExpenseItem;
+// props == properties from app.js
