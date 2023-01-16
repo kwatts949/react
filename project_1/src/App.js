9546c8d578f3938747049c57d4cc0a4fc9d5d71f
Kayleigh Watts <kwatts949@gmail.com>
2023-01-16 09:52:06 +0000

    Add expense item component

--- project_1/src/App.js
+++ project_1/src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>My App</p>
+        <h1>My App</h1>
+        <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
