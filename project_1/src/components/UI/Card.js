02967a32e13189e86f298b3e3cf3eb6e850e8409
Kayleigh Watts <kwatts949@gmail.com>
2023-01-17 16:09:39 +0000

    Remove old dir

--- project_1/src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-const Card = (props) => {
-  const classes = 'card ' + props.className
-  return <div className={classes}>{props.children}</div>;
-};
-
-export default Card;
-
-// children is the content between the opening and closing tags of wrapper
